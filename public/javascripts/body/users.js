e036ef87a48fd90c59f6dfa326f21c1cf3b3ec65
Haim Greenstein <haim763@gmail.com>
2017-10-31 21:02:03 +0200

    start of moving the user table generation to the client side.

--- public/javascripts/body/users.js
+++ public/javascripts/body/users.js
@@ -4,16 +4,45 @@ function onSelectId(id) {
     selectedId = id;
     $('.selected-id').text('' + id);
 }
+
 function onDeleteClick(){
     const id = selectedId;
-    $.ajax({
-        url: './rest/User/' + id,
+    $.ajax('./rest/User/' + id, {
         type: "DELETE",
-        success: function () {
-            refreshBody()
-        },
-        error: function (x) {
-            console.log(x.responseText)
+        success: refreshUsers,
+        error: onError
+    });
+}
+
+
+function onError (x) {
+    console.log(x.responseText)
+}
+
+function refreshUsers() {
+    $.ajax('./rest/User', {
+        success: showUsers,
+        error: onError
+    })
+}
+
+function onSelectId(id) {
+    throw Error("not implemented")
+}
+
+function showUsers(users) {
+    throw Error("not implemented")
+    // TODO: define: props, editable.
+    users.forEach(user => {
+        const id = user.id;
+        const tr = $('#usersTable').find('tbody').append('tr');
+        tr.append('<td><input type="checkbox" class="checkthis"></td>');
+        props.forEach(field => {
+            tr.append('<td>' + obj[field] + '</td>');
+        });
+        if (editable) {
+            tr.append('<td><p data-placement="top" data-toggle="tooltip" title="" data-original-title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" onclick="onSelectId('+id+')"><span class="glyphicon glyphicon-pencil"></span></button></p></td>');
+            tr.append('<td><p data-placement="top" data-toggle="tooltip" title="" data-original-title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" onclick="onSelectId('+id+')"><span class="glyphicon glyphicon-trash"></span></button></p></td>');
         }
     });
 }
--- routes/rest.js
+++ routes/rest.js
@@ -2,14 +2,36 @@ const express = require('express');
 const router = express.Router();
 const db = require('../model/database');
 
+function wrapJson(json) {
+    return { data: json }
+}
 
- router.delete('/:entity/:id', (req, res, next) => {
-     const id = req.params.id;
-     const entity = req.params.entity;
-     const deleteResult = db.deleteEntity(entity, id);
-     if (deleteResult instanceof Error) next(deleteResult);
-     res.status(201).json({});
- });
+
+router.delete('/:entity/:id', (req, res, next) => {
+    const id = req.params.id;
+    const entity = req.params.entity;
+
+    db.deleteEntity(entity, id);
+
+    res.status(201).json({});
+});
+
+router.get('/:entity', (req,res,next) => {
+    const entity = req.params.entity;
+
+    const items = db.getEntities(entity);
+
+    res.status(201).json({items: items});
+});
+
+router.get('/:entity/:id', (req,res,next) => {
+    const id = req.params.id;
+    const entity = req.params.entity;
+
+    const item = db.getEntity(entity,id);
+
+    res.status(201).json({item: item});
+});
 
 
 module.exports = router;
